e310595611e9af72646fc5f4623f7f1a98da7c69
Jugraj Singh <jsbains96@gmail.com>
2023-08-29 14:49:28 +0100

    adding links to the icons

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,35 @@
+import { Link } from "react-router-dom";
 import "./Nav.scss";
+import Home from "../../assets/icons/Home.svg";
+import Events from "../../assets/icons/Events.svg";
+import Calendar from "../../assets/icons/Calendar.svg";
+import About from "../../assets/icons/About.svg";
 
 const Nav = () => {
   return (
     <>
       <nav className="Nav">
-        <ul>
-          <li>
-            <img src="../../assets/icons/Home.svg" alt="Home link" />
-          </li>
-          <li>
-            <img src="../../assets/icons/Events.svg" alt="Events link" />
-          </li>
-          <li>
-            <img src="../../assets/icons/Calendar.svg" alt="Calendar link" />
-          </li>
-          <li>
-            <img src="../../assets/icons/About.svg" alt="About link" />
-          </li>
+        <ul className="Nav__image">
+          <Link to={"/"}>
+            <img src={Home} alt="Home link" className="Nav__image--home" />
+          </Link>
+          <Link to={""}>
+            <img
+              src={Events}
+              alt="Events link"
+              className="Nav__image--events"
+            />
+          </Link>
+          <Link to={""}>
+            <img
+              src={Calendar}
+              alt="Calendar link"
+              className="Nav__image--calendar"
+            />
+          </Link>
+          <Link to={""}>
+            <img src={About} alt="About link" className="Nav__image--about" />
+          </Link>
         </ul>
       </nav>
     </>
