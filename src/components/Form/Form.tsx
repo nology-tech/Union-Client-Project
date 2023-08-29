419fed74e95a03f396ab2ca4f4708e11893a596b
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-29 15:16:21 +0100

    Changes

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,14 @@
+import "./Form.scss";
+
+type FormProps = {};
+
+const Form = ({}: FormProps) => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Form;
