e8a2529cb17e9cc437cfe68e3bce93a37d634398
Chu <Chu@github.com>
2023-08-30 11:35:38 +0100

    Header finished - added location prop for use in home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,30 @@ type HeaderProps = {
   subTitle?: string;
   imageUrl?: string;
   videoUrl?: string;
+  locationVenue?: string;
+  locationCity?: string;
 };
 
-const Header = ({ title, subTitle, imageUrl, videoUrl }: HeaderProps) => {
+const Header = ({
+  title,
+  subTitle,
+  imageUrl,
+  videoUrl,
+  locationVenue,
+  locationCity,
+}: HeaderProps) => {
   return (
     <div className="header">
       <div className="header__content">
         <h4 className="header__brand">DOT TO DOT - LOCAL MAKERS</h4>
         <h1 className="header__title">{title}</h1>
         <p className="header__subtitle">{subTitle}</p>
+        {locationVenue && locationCity && (
+          <p className="header__location">
+            <span className="header__location--venue">{`${locationVenue}, `}</span>
+            {locationCity}
+          </p>
+        )}
       </div>
       {imageUrl && (
         <div className="header__image-positioning">
