64c0ad7c8466c5b80e2fa43bfd1b548788452238
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-30 14:43:29 +0100

    created SplashPage Page with working buttons and styles.

---
+++ src/pages/SplashPage/SplashPage.tsx
@@ -0,0 +1,48 @@
+import "./SplashPage.scss";
+import Button from "../../components/Button/Button";
+import paintingWoman from "../../images/paintingWoman.png";
+import { Link } from "react-router-dom";
+
+const SplashPage = () => {
+  const dummyClick = () => {};
+
+  return (
+    <div className="splashPage">
+      <div className="splashPage--imageContainer">
+        <img src={paintingWoman} />
+      </div>
+      <div className="splashPage--textContainer">
+        <div>
+          <p className="splashPage--textContainer__line1">
+            DOT TO DOT - LOCAL MAKERS
+          </p>
+        </div>
+        <div>
+          <p className="splashPage--textContainer__line2">
+            Made by Makers <br></br> Studio Tour
+          </p>
+        </div>
+        <div>
+          <p className="splashPage--textContainer__line3">
+            Nov - Dec 2021 <br></br> <span className="bold">Paintworks,</span>{" "}
+            Bristol
+          </p>
+        </div>
+      </div>
+      <div
+        className="splashPage--buttonsContainer"
+        id="splashPage--buttonsContainerId"
+      >
+        <Link style={{ textDecoration: "none" }} to="/sign-up">
+          <Button label="create an account" onClick={dummyClick} />
+        </Link>
+
+        <Link style={{ textDecoration: "none" }} to="/login">
+          <Button label="sign in" variant="secondary" onClick={dummyClick} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SplashPage;
