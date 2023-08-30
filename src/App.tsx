30b767cabfb49fed2b633045867bded4e111c4f6
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-30 09:35:15 +0100

    added dummy form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <div>
+      <Form />
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,67 @@
+import { useState } from "react";
 import "./Form.scss";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
-type FormProps = {};
+const Form = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-const Form = ({}: FormProps) => {
+  const [user, setUser] = useState({});
+
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  // const handleInput = async (event) => {
+  //   setEmail(event.currentTarget.value);
+  //   setPassword(event.currentTarget.value);
+
+  //   signInWithEmailAndPassword(auth, email, password);
+
+  //   // Signed in
+  //   const userCredential = await signInWithEmailAndPassword(
+  //     auth,
+  //     email,
+  //     password
+  //   );
+  //   const user = userCredential.user;
+  //   // ...
+  //   console.log(user);
+  // };
+
+  // console.log(email, password);
+
   return (
     <div>
-      <input type="text" />
-      <input type="text" />
+      <label htmlFor="email">email</label>
+      <input
+        type="text"
+        id="email"
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <label htmlFor="password">password</label>
+      <input
+        type="text"
+        id="password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button type="submit" onClick={handleLogin}>
+        Submit
+      </button>
     </div>
   );
 };
