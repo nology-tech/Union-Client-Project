f2d77bd21a91703ff067d2df2674e8815942717d
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-30 15:27:04 +0100

    added route and path in app.tsx also made Nav conditionally render on pages not the splash page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 import Nav from "./components/Nav/Nav";
+import SplashPage from "./pages/SplashPage/SplashPage";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<SplashPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/events" element={""} />
         <Route path="/calendar" element={""} />
         <Route path="/about" element={""} />
       </Routes>
-      <Nav />
+      {window.location.pathname !== "/" && <Nav />}
     </>
   );
 };
