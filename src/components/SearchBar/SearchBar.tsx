99c868945e4d89bcd3eba305b78a673d61833cad
lucaGilardenghi <luca.gilardenghi@yahoo.co.uk>
2023-08-30 15:19:00 +0100

    created searchBar component & props

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,26 @@
+import "./SearchBar.scss";
+import search from "../../assets/icons/eva_search-fill.svg";
+
+type SearchBarProps = {
+  searchEvents: string;
+  handleInput: () => void;
+};
+
+const SearchBar = ({ handleInput, searchEvents }: SearchBarProps) => {
+  return (
+    <>
+      <div className="search">
+        <img src={search} className="search__image" />
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Search by craft, material, discipline.."
+          value={searchEvents}
+          onInput={handleInput}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
