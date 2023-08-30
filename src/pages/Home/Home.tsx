806e1b8a6daeeae49e5721ef919bd6d781c716ce
Max <maxswaine@gmail.com>
2023-08-30 12:24:44 +0100

    initial commit - basic content loaded

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,60 @@
+import Button from "../../components/Button/Button";
 import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
   return (
-    <div>
-      <Layout>
-        <h1>Page Heading</h1>
-        <h2>Section Heading</h2>
-        <h3>Panel Heading</h3>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-          dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-          maxime tempora, temporibus beatae voluptas repellat rerum.
-          Dignissimos, necessitatibus.
-        </p>
-      </Layout>
-    </div>
+    <Layout>
+      <Button label="View Events" />
+      <h1>If this is your first Made by makers for the people, welcome!</h1>
+      <p>
+        Made by the people for the people. A local studio tour of local makers
+        around the area of bristol. Come see the workshops where the magic happ
+        Stop for a quick sketch and pot of tea and pastry from the pottery
+        studio.
+      </p>
+      <br></br>
+      <br></br>
+      <p>
+        Make something from the local craft shop. Explore the amazing works of
+        art from a number of craft artists. Make it yourself from ceramics and
+        other hand made materials. Sign up for an excursion to the city where
+        you will learn how pottery is made at the Bristol Pottery Workshop. All
+        activities are free and all the pottery is for sale. The tour is
+        organized and presented by Chelsea Handmade & Local.
+      </p>
+      <h2>
+        A note from our Principal Organiser, Steven Devine, about this weekends
+        programme:
+      </h2>
+      <p>
+        Local producer of bath and beauty products. Check out the bath and
+        beauty products line from this maker. Located in oak barrels. Make
+        yourself some bath salts and use the handmade containers for a nice
+        relaxing bath at home.
+      </p>
+      <br></br>
+      <br></br>
+      <p>
+        Make some small pots with creative molding techniques. Tryens. making
+        some pieces with the pottery tools.
+      </p>
+      <br></br>
+      <br></br>
+      <p>
+        Visit a local art store and get some handmade brushes. You can paint
+        your own wall at the studio where you’ll get a tour of the studio and
+        you can choose a souvenir tile to make yourself a unique decorative
+        piece in your house.
+      </p>
+      <br></br>
+      <br></br>
+      <p>
+        Learn more about the history of pottery and how ceramics can be used as
+        a building material for small clay bricks. Using the tools you learned
+        from the workshop you will make a few pieces.
+      </p>
+      <Button label="ABOUT" />
+    </Layout>
   );
 };
 
