c417e3b30e759856ef64647c721e491388afa3e9
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-30 12:26:08 +0100

    Added error page with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path="/error" element={<Error />} />
     </Routes>
   );
 };
--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,24 @@
 import "./Error.scss";
+import ErrorImage from "../../assets/images/404Error.png";
+import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/home");
+  };
+
   return (
-    <div>
-      <h1>OOPS. LOOKS LIKE YOU'VE TAKEN A WRONG TURN</h1>
+    <div className="error">
+      <h1 className="error__heading">
+        OOPS. <br /> LOOKS LIKE YOU'VE TAKEN A WRONG TURN.
+      </h1>
+      <img className="error__image" src={ErrorImage} alt="Error Image" />
+      <div className="error__button">
+        <Button label="Home" onClick={handleClick} />
+      </div>
     </div>
   );
 };
