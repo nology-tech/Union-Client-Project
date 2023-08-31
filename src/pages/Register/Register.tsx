a0b76211990f7c5969a1b39136e81c7e751b0b72
Tufiale <tufiale@gmail.com>
2023-08-31 15:40:08 +0100

    completed styles and obtained values from input boxes

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,6 +3,10 @@ import "./Register.scss";
 import { FirebaseError } from "firebase/app";
 import { auth } from "../../firebase";
 import arrow from "../../images/arrow.png";
+import InputBox from "../../components/InputBox/InputBox";
+import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
+import { FormEvent } from "react";
 
 type RegisterProps = {
   email: string;
@@ -10,9 +14,17 @@ type RegisterProps = {
   password: string;
   setPassword: (password: string) => void;
   setUserId: (userId: string) => void;
+  firstName: string;
+  lastName: string;
+  setFirstName: (firstName: string) => void;
+  setLastName: (lastName: string) => void;
 };
 
 const Register = ({
+  firstName,
+  lastName,
+  setFirstName,
+  setLastName,
   email,
   setEmail,
   password,
@@ -33,15 +45,47 @@ const Register = ({
       }
     }
   };
+  const handleClickNext = () => {
+    navigate("/register/2");
+  };
+  const navigate = useNavigate();
+
+  const handleFirstName = (event: FormEvent<HTMLInputElement>) => {
+    setFirstName(event.currentTarget.value);
+  };
+
+  const handleLastName = (event: FormEvent<HTMLInputElement>) => {
+    setLastName(event.currentTarget.value);
+  };
 
   return (
     <div className="register-page">
       <div className="image-container">
         <img className="image-container__image" src={arrow} alt="" />
       </div>
-
       <div className="register-page__heading">
-        <h1>Create An Account</h1>
+        <h1 className="register-page__heading__header">Create An Account</h1>
+      </div>
+      <div className="register-page__input">
+        <div className="register-page__first-name">
+          <InputBox
+            label="First Name"
+            inputType="text"
+            inputPlaceholder="  John"
+            handleInput={handleFirstName}
+          />
+        </div>
+        <div className="register-page__last-name">
+          <InputBox
+            label="Last Name"
+            inputType="text"
+            inputPlaceholder="  Doe"
+            handleInput={handleLastName}
+          />
+        </div>
+        <div className="register-page__next-button">
+          <Button label="Next" onClick={handleClickNext} />
+        </div>
       </div>
     </div>
   );
