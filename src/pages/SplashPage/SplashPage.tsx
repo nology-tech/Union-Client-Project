b2f13ea2f831da308d9376101cec275737234bb0
Tufiale <tufiale@gmail.com>
2023-08-31 10:33:03 +0100

    corrected BEM, implemented kebab-casing where necessary and added useNaviagate function for buttons comonent

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,43 +1,50 @@
 import "./SplashPage.scss";
 import Button from "../../components/Button/Button";
-import paintingWoman from "../../images/paintingWoman.png";
-import { Link } from "react-router-dom";
+import painting from "../../images/painting-woman.png";
+import { useNavigate } from "react-router-dom";
 
 const SplashPage = () => {
+  const navigate = useNavigate();
+
+  const handleClickRegister = () => {
+    navigate("/register");
+  };
+
+  const handleClickLogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <div className="splashPage">
-      <div className="splashPage__imageContainer">
-        <img src={paintingWoman} />
+    <div className="splash-page">
+      <div className="splash-page__image-container">
+        <img src={painting} className="splash-page__image" />
       </div>
-      <div className="splashPage__textContainer">
+      <div className="splash-page__text-container">
         <div>
-          <p className="splashPage__textContainer splashPage__textContainer--line1">
+          <p className="splash-page__text-container splash-page__text-container--line-1">
             DOT TO DOT - LOCAL MAKERS
           </p>
         </div>
         <div>
-          <p className="splashPage__textContainer splashPage__textContainer--line2">
+          <p className="splash-page__text-container splash-page__text-container--line-2">
             Made by Makers <br></br> Studio Tour
           </p>
         </div>
         <div>
-          <p className="splashPage__textContainer splashPage__textContainer--line3">
+          <p className="splash-page__text-container splash-page__text-container--line-3">
             Nov - Dec 2021 <br></br> <span className="bold">Paintworks,</span>{" "}
             Bristol
           </p>
         </div>
       </div>
-      <div
-        className="splashPage__buttonsContainer"
-        id="splashPage__buttonsContainerId"
-      >
-        <Link style={{ textDecoration: "none" }} to="/register">
-          <Button label="create an account" />
-        </Link>
+      <div className="splash-page__buttons-container">
+        <Button label="create an account" onClick={handleClickRegister} />
 
-        <Link style={{ textDecoration: "none" }} to="/login">
-          <Button label="sign in" variant="secondary" />
-        </Link>
+        <Button
+          label="sign in"
+          variant="secondary"
+          onClick={handleClickLogin}
+        />
       </div>
     </div>
   );
