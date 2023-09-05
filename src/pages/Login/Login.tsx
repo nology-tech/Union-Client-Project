76a43cd9f0c3c68109aa60323366b4a0115dc2a5
Nicholas Phelan <125403716+nmWolfe@users.noreply.github.com>
2023-09-05 15:30:45 +0100

    17 feature federated sign in (#26)
    * adding to change branch
    
    * Design finished
    
    * removed component from login
    
    * removed import
    
    * 16 feature register page (#18)
    
    * Added input box component
    
    * committing initial register page/styles
    
    * completed styles and obtained values from input boxes
    
    * modified button functionality
    
    * added toggle feature, still in construction
    
    * added toggle, computer issue mus restart.
    
    * added toggle feature to button and back arrow.
    
    * added colorChange usestate for password check.
    
    * modified logic for password input boxes, confirm password also has user feedback if not correct.
    
    * modified the style for confirm password.
    
    * altered logic for colorChange.
    
    * deleted some old code.
    
    * refactored code to make conditional render easier to read and cleaner.
    
    * removed variables called but not read.
    
    * added checks to make sure email is formatted properly and user feedback is presented.
    
    * added 3 tests to test for basic rendering on the page.
    
    * added basic readme.md
    
    * updated readme
    
    * changed toggle to handleClickNext
    
    * altered useState locations from app into register, changed SCSS naming (header).
    
    * altered test file to remove references to first and last name.
    
    ---------
    
    Co-authored-by: Nick <n.m.phelan@gmail.com>
    Co-authored-by: Tufiale <tufiale@gmail.com>
    Co-authored-by: Luke Welbourn <luke.tarquin5@outlook.com>
    
    * Added GHSec to build YAML (#22)
    
    * Added GHSec to build YAML
    
    * updated all yml files with env.
    
    * Updated env, and firebase.ts with API key
    
    * Updated API var name to VITE, as REACT was causing page load to fail
    
    * Added env to gitignore
    
    * deleting env
    
    * Prefixing VITE to env variables
    
    * Fix firebase.ts
    
    ---------
    
    Co-authored-by: Charlie Richardson <55165280+Charlie-robin@users.noreply.github.com>
    Co-authored-by: charlie-robin <charlie.robin.richardson@gmail.com>
    
    * commiting to get updated main
    
    * adding for pull
    
    * added .env file
    
    * added google federated sign in
    
    * added basic provider code and functions.
    
    * pop up added for facebook and apple login. Added fedSignIn for register page and login page.
    
    * added basic rendering test to component file, added error handling in component.
    
    * removed env file.
    
    ---------
    
    Co-authored-by: luke-welbourn <137337707+luke-welbourn@users.noreply.github.com>
    Co-authored-by: Tufiale <tufiale@gmail.com>
    Co-authored-by: Luke Welbourn <luke.tarquin5@outlook.com>
    Co-authored-by: Charlie Richardson <55165280+Charlie-robin@users.noreply.github.com>
    Co-authored-by: charlie-robin <charlie.robin.richardson@gmail.com>

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,14 @@
 import "./Login.scss";
 import InputBox from "../../components/InputBox/InputBox";
-import Button from "../../components/Button/Button"
+import Button from "../../components/Button/Button";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { FirebaseError } from "firebase/app";
 import { auth } from "../../firebase";
 import { ChangeEvent } from "react";
-import arrow from '../../assets/images/arrow.png'
+import arrow from "../../assets/images/arrow.png";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import FedSignIn from "../../components/FedSignIn/FedSignIn";
 
 type LoginProps = {
   email: string;
@@ -17,27 +18,25 @@ type LoginProps = {
   setUserId: (userId: string) => void;
 };
 
-
 const Login = ({
   email,
   setEmail,
   password,
   setPassword,
-  setUserId
+  setUserId,
 }: LoginProps) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [loginError, setLoginError] = useState<boolean>(false);
 
   const handleLogin = async () => {
     try {
       const userData = await signInWithEmailAndPassword(auth, email, password);
       setUserId(userData.user.uid);
-      navigate("/home")
+      navigate("/home");
     } catch (error: unknown) {
       if (error instanceof FirebaseError) {
         console.error(error.code);
         setLoginError(true);
-
       }
     }
   };
@@ -53,7 +52,9 @@ const Login = ({
   return (
     <div className="login-page">
       <div className="image-container">
-        <Link to={"/"}><img className="image-container__image" src={arrow} alt="" /></Link>
+        <Link to={"/"}>
+          <img className="image-container__image" src={arrow} alt="" />
+        </Link>
       </div>
       <div className="login-page__heading">
         <h1 className="login-page__heading--header">Welcome Back</h1>
@@ -64,13 +65,23 @@ const Login = ({
             Sorry, we don't recognise that login
           </p>
         )}
-        <InputBox label="Email Address" inputPlaceholder="you@example.com" inputType="text" handleInput={handleEmailInput} />
-        <InputBox label="Password" inputPlaceholder="Your password" inputType="password" handleInput={handlePasswordInput} />
-
+        <InputBox
+          label="Email Address"
+          inputPlaceholder="you@example.com"
+          inputType="text"
+          handleInput={handleEmailInput}
+        />
+        <InputBox
+          label="Password"
+          inputPlaceholder="Your password"
+          inputType="password"
+          handleInput={handlePasswordInput}
+        />
       </div>
       <div className="login-page__button-container">
-        <Button label="SIGN IN" onClick={handleLogin}/>
+        <Button label="SIGN IN" onClick={handleLogin} />
       </div>
+      <FedSignIn setUserId={setUserId} />
     </div>
   );
 };
