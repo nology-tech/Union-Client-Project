f48a7edb3b69335d17b4efb37c341aa8d5b2b870
Jugraj Singh <jsbains96@gmail.com>
2023-09-05 12:41:13 +0100

    Pull request fixes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,11 @@ import SplashPage from "./pages/SplashPage/SplashPage";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Events from "./pages/Events/Events";
-import { mockEvents } from "./data/mockEvents";
 import About from "./pages/About/About";
 import Calendar from "./pages/Calendar/Calendar";
 
 const App = () => {
-  const [userId, setUserId] = useState<string>("true");
+  const [userId, setUserId] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
@@ -22,7 +21,7 @@ const App = () => {
         {userId ? (
           <>
             <Route path="/home" element={<Home />} />
-            <Route path="/events" element={<Events eventData={mockEvents} />} />
+            <Route path="/events" element={<Events />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/about" element={<About />} />
           </>
