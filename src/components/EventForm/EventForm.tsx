2a693f8aba2aa4f1a12946abbeceb24a69a3ce08
Nick <n.m.phelan@gmail.com>
2023-09-06 14:28:54 +0100

    changed input to textarea for description

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -2,32 +2,68 @@ import { ChangeEvent } from "react";
 import Button from "../Button/Button";
 import InputBox from "../InputBox/InputBox";
 import "./EventForm.scss";
-import { doc, addDoc } from "firebase/firestore";
+import { setDoc, doc, Timestamp } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useState } from "react";
 
 const EventForm = () => {
+  const [databaseInput, setDatabaseInput] = useState({
+    eventName: "",
+    eventCategory: "",
+    eventCapacity: 0,
+    eventDescription: "",
+    eventDate: Timestamp,
+    eventImages: [""],
+  });
+
   const handleEventName = (event: ChangeEvent<HTMLInputElement>) => {
     const eventName = event.currentTarget.value;
+    setDatabaseInput({ ...databaseInput, eventName: eventName });
   };
   const handleEventCategory = (event: ChangeEvent<HTMLInputElement>) => {
     const eventCategory = event.currentTarget.value;
+    setDatabaseInput({ ...databaseInput, eventCategory: eventCategory });
   };
   const handleEventDate = (event: ChangeEvent<HTMLInputElement>) => {
-    const eventDate = event.currentTarget.value;
+    const date = event.currentTarget.value;
+    setDatabaseInput({ ...databaseInput, eventDate: Number(date) });
   };
-  const handleEventCapactiy = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleEventCapacity = (event: ChangeEvent<HTMLInputElement>) => {
     const eventCapacity = event.currentTarget.value;
+    setDatabaseInput({
+      ...databaseInput,
+      eventCapacity: Number(eventCapacity),
+    });
   };
-  const handleEventDescription = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleEventDescription = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const eventDescription = event.currentTarget.value;
+    setDatabaseInput({ ...databaseInput, eventDescription: eventDescription });
+  };
+  const handleEventImages = (event: ChangeEvent<HTMLInputElement>) => {
+    const images = event.currentTarget.value;
+    setDatabaseInput({ ...databaseInput, eventImages: [images] });
   };
 
-  const handleSubmit = () => {
-    return;
+  const handleSubmit = async () => {
+    try {
+      const docRef = await setDoc(doc(db, "events", databaseInput.eventName), {
+        name: databaseInput.eventName,
+        capacityMax: databaseInput.eventCapacity,
+        category: databaseInput.eventCategory,
+        description: databaseInput.eventDescription,
+        date: databaseInput.eventDate,
+        capacityCurrent: 0,
+        images: databaseInput.eventImages,
+      });
+      console.log(docRef);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     // turn this into a number
-    <div>
+    <div className="event-form">
       <InputBox
         label="Event Name (case sensitive)"
         handleInput={handleEventName}
@@ -38,22 +74,32 @@ const EventForm = () => {
         handleInput={handleEventCategory}
         inputType="text"
       />
-      <InputBox label="Date" handleInput={handleEventDate} inputType="text" />
+      <InputBox label="Date" handleInput={handleEventDate} inputType="date" />
       <InputBox
         label="Max Capacity"
-        handleInput={handleEventCapactiy}
+        handleInput={handleEventCapacity}
         inputType="text"
+      />
+      <label
+        htmlFor="event-description"
+        className="event-form__event-description--label"
+      >
+        Event Description
+      </label>
+      <textarea
+        name="Event Description"
+        id="event-description"
+        cols="30"
+        rows="10"
+        placeholder="Describe your event"
+        onChange={handleEventDescription}
+        className="event-form__event-description"
       />
       <InputBox
-        label="Event Description (case sensitive)"
-        handleInput={handleEventDescription}
+        label="Image URL"
+        handleInput={handleEventImages}
         inputType="text"
       />
-      {/* <InputBox
-        label="Image URL"
-        handleInput={somethingHere}
-        inputType="text"
-      /> */}
       <div className="event-form__button">
         <Button label="Submit" onClick={handleSubmit} />
       </div>
