ab7883d7228810720ede9510f5f8d88515f7e8b4
Nick <n.m.phelan@gmail.com>
2023-09-06 10:13:06 +0100

    Removed offending files from test files

--- src/pages/Register/Register.test.tsx
+++ src/pages/Register/Register.test.tsx
@@ -4,37 +4,20 @@ import "./Register";
 import { customRender } from "../../utils/testUtils";
 import Register from "./Register";
 
-const fakeString = "fake";
 const fakeFunction = () => {
   const anotherFake = "fake";
   return anotherFake;
 };
 
 it("should render the Header", () => {
-  customRender(
-    <Register
-      email={fakeString}
-      password={fakeString}
-      setEmail={fakeFunction}
-      setPassword={fakeFunction}
-      setUserId={fakeFunction}
-    />
-  );
+  customRender(<Register setUser={fakeFunction} />);
   const header = screen.getByText("Create An Account");
 
   expect(header).toBeInTheDocument();
 });
 
 it("should render the back arrow image", () => {
-  customRender(
-    <Register
-      email={fakeString}
-      password={fakeString}
-      setEmail={fakeFunction}
-      setPassword={fakeFunction}
-      setUserId={fakeFunction}
-    />
-  );
+  customRender(<Register setUser={fakeFunction} />);
 
   const image = screen.getByAltText("back-arrow");
 
@@ -42,15 +25,7 @@ it("should render the back arrow image", () => {
 });
 
 it("should render both input boxes", () => {
-  customRender(
-    <Register
-      email={fakeString}
-      password={fakeString}
-      setEmail={fakeFunction}
-      setPassword={fakeFunction}
-      setUserId={fakeFunction}
-    />
-  );
+  customRender(<Register setUser={fakeFunction} />);
 
   const firstName = screen.getByPlaceholderText("John");
 
