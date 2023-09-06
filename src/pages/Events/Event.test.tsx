125d9e2a87a8b0d4f20456be5f6307a1efd53f49
Tufiale <tufiale@gmail.com>
2023-09-06 09:51:50 +0100

    Modified Events.test.tsx to use dbData

--- src/pages/Events/Event.test.tsx
+++ src/pages/Events/Event.test.tsx
@@ -3,16 +3,29 @@ import Events from "./Events";
 import userEvents from "@testing-library/user-event";
 import { customRender } from "../../utils/testUtils";
 import { getEvents } from "../../utils/firebaseSnapshots";
+import { Event } from "../../types/types";
+import { useState, useEffect } from "react";
+
+const [dbData, setDbData] = useState<Event[]>([]);
+
+useEffect(() => {
+  getDbData();
+}, []);
+
+const getDbData = async () => {
+  const data = await getEvents();
+  setDbData(data as Event[]);
+};
 
 it("should render all the events once page loads", () => {
-  customRender(<Events />);
+  customRender(<Events eventData={dbData} />);
 
   const eventCardButtons = screen.getAllByTestId(/event-card/i);
   expect(eventCardButtons.length).toBe(getEvents.length);
 });
 
 it("should render only 1 related event once beer has been searched", async () => {
-  customRender(<Events />);
+  customRender(<Events eventData={dbData} />);
 
   const searchBar = screen.getByRole("textbox");
   await userEvents.type(searchBar, "beer");
@@ -22,7 +35,7 @@ it("should render only 1 related event once beer has been searched", async () =>
 });
 
 it("should not render any events if search not matched", async () => {
-  customRender(<Events />);
+  customRender(<Events eventData={dbData} />);
 
   const searchBar = screen.getByRole("textbox");
   await userEvents.type(searchBar, "cooking");
