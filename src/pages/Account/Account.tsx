531b226692118ab82873322feb3e66ef0bcdb5b2
Jugraj Singh <jsbains96@gmail.com>
2023-09-06 10:16:22 +0100

    Added the styling and content to the accounts page

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,18 +1,49 @@
 import Layout from "../../components/Layout/Layout";
 import "./Account.scss";
 import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+import { MouseEvent } from "react";
+import placeHolderPFP from "/src/assets/images/placeHolderPFP.svg";
 
 const Account = () => {
-
   return (
     <Layout>
-      <Header
-        title={"Welcome Back"}
-        subTitle="Name goes here..."
-        
-        />
+      <Header title={"Welcome Back"} />
       <div className="account-page">
-        <h1>Page under development.</h1>
+        <div className="account-page__content">
+          <div className="account-page__content--photo">
+            <img
+              className="account-page__content--image"
+              src={placeHolderPFP}
+              alt="your photo"
+            />
+          </div>
+          <div className="account-page__text-content">
+            <h1 className="account-page__text-content--first-name">First Name</h1>
+            <p className="account-page__text-content--name">Jugraj</p>
+            <div className="account-page__text-content--box"></div>
+            <h1 className="account-page__text-content--last-name">Last Name</h1>
+            <p className="account-page__text-content--name">Singh</p>
+            <div className="account-page__text-content--box"></div>
+            <h1 className="account-page__text-content--email">
+              Email
+            </h1>
+            <p className="account-page__text-content--email-address">
+              testing@yahoo.co.uk
+            </p>
+            <div className="account-page__text-content--box"></div>
+          </div>
+        </div>
+        <div className="sign-out-button">
+          <Button
+            label={"Sign Out"}
+            onClick={function (
+              event: MouseEvent<HTMLButtonElement, MouseEvent>
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        </div>
       </div>
     </Layout>
   );
