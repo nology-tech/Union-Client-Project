69eecd33e893f7d078864a304c8ed6ca25bc99d5
Jugraj Singh <jsbains96@gmail.com>
2023-09-06 12:34:23 +0100

    fixed another pr fail

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -4,7 +4,6 @@ import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import placeHolderPFP from "/src/assets/images/placeHolderPFP.svg";
 import InputBox from "../../components/InputBox/InputBox";
-import { ChangeEvent } from "react";
 
 const Account = () => {
   const handleSignOut = () => {
