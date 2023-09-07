be9a8550984e6f0219522e77742edd7380a32da4
Nick <n.m.phelan@gmail.com>
2023-09-07 16:06:22 +0100

    added most functionality, current push error

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -9,7 +9,7 @@ import "./EventForm.scss";
 type DefaultForm = {
   eventName: string;
   eventCategory: string;
-  eventCapacity: number;
+  eventCapacity: number | null;
   eventDescription: string;
   eventImages: string[];
   eventDate: Timestamp | null;
@@ -19,13 +19,21 @@ const EventForm = () => {
   const [databaseInput, setDatabaseInput] = useState<DefaultForm>({
     eventName: "",
     eventCategory: "",
-    eventCapacity: 0,
+    eventCapacity: null,
     eventDescription: "",
     eventDate: null,
     eventImages: [""],
   });
   const [images, setImages] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
+  const [required, setRequired] = useState({
+    errorOne: false,
+    errorTwo: false,
+    errorThree: false,
+    errorFour: false,
+    errorFive: false,
+    errorSix: false,
+  });
 
   const handleEventName = (event: ChangeEvent<HTMLInputElement>) => {
     const eventName = event.currentTarget.value;
@@ -46,9 +54,18 @@ const EventForm = () => {
   };
   const handleEventCapacity = (event: ChangeEvent<HTMLInputElement>) => {
     const eventCapacity = event.currentTarget.value;
+    if (
+      !eventCapacity.toLowerCase().search(/[a-z]/g || eventCapacity.length < 1)
+    ) {
+      setDatabaseInput({
+        ...databaseInput,
+        eventCapacity: null,
+      });
+      return;
+    }
     setDatabaseInput({
       ...databaseInput,
-      eventCapacity: Number(eventCapacity),
+      eventCapacity: parseInt(eventCapacity),
     });
   };
   const handleEventDescription = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -67,11 +84,35 @@ const EventForm = () => {
     setDatabaseInput({ ...databaseInput, eventImages: [...images, image] });
     setInputValue("");
   };
-
+  const handleRemoveImage = () => {
+    if (images.length > 0) {
+      images.pop();
+    }
+    setImages((images) => [...images]);
+  };
   const handleInputValue = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.currentTarget.value);
   };
+  const handleRequirements = () => {
+    const currentErrors = {
+      errorOne: databaseInput.eventName.length < 1,
+      errorTwo: databaseInput.eventCategory.length < 1,
+      errorThree: !databaseInput.eventDate,
+      errorFour: databaseInput.eventCapacity === null,
+      errorFive: databaseInput.eventDescription.length < 1,
+      errorSix: databaseInput.eventImages.length === 0,
+    };
+    if (!Object.values(required)) {
+      console.log("clicked");
 
+      handleSubmit();
+    } else {
+      console.log("clicked else");
+      console.log(required);
+
+      setRequired(currentErrors);
+    }
+  };
   const handleSubmit = async () => {
     try {
       await addDoc(collection(db, "events-test"), {
@@ -87,26 +128,37 @@ const EventForm = () => {
       console.error(error);
     }
   };
+  const buttonStyle = {
+    width: "5rem",
+    height: "3rem",
+    margin: "1rem",
+  };
+  const errorRequired = (
+    <p className="event-form__required">this field is required</p>
+  );
 
   return (
-    // turn this into a number
     <div className="event-form">
       <InputBox
         label="Event Name (case sensitive)"
         handleInput={handleEventName}
         inputType="text"
       />
+      {required.errorOne && errorRequired}
       <InputBox
-        label="Category (case sensitive)"
+        label="Event Category (case sensitive)"
         handleInput={handleEventCategory}
         inputType="text"
       />
+      {required.errorTwo && errorRequired}
       <InputBox label="Date" handleInput={handleEventDate} inputType="date" />
+      {required.errorThree && errorRequired}
       <InputBox
-        label="Max Capacity"
+        label="Event Capacity"
         handleInput={handleEventCapacity}
         inputType="text"
       />
+      {required.errorFour && errorRequired}
       <label
         htmlFor="event-description"
         className="event-form__event-description--label"
@@ -122,6 +174,7 @@ const EventForm = () => {
         onChange={handleEventDescription}
         className="event-form__event-description"
       />
+      {required.errorFive && errorRequired}
       <label
         htmlFor="image__input"
         className="event-form__event-description--label"
@@ -135,21 +188,40 @@ const EventForm = () => {
         value={inputValue}
         onChange={handleInputValue}
       />
-      <div className="event-form__images">
+      {required.errorSix && errorRequired}
+      <div className="event-form__images--button">
         <Button
           label="Add Image"
           onClick={handleAddImage}
+          style={buttonStyle}
+        />
+        <Button
+          label="Remove Image"
+          onClick={handleRemoveImage}
           variant="secondary"
+          style={buttonStyle}
         />
       </div>
-      {images.length > 0 && (
-        <p className="event-form__image-response">
-          {images.length} images added
-        </p>
-      )}
+      <div className="event-form__gallery">
+        {images.length > 0 &&
+          images.map((image, index) => {
+            return (
+              <div
+                className="event-form__gallery--images"
+                key={`event-form__${index}`}
+              >
+                <img
+                  className="event-form__gallery--image"
+                  src={image}
+                  alt={`event-form__image-${index}`}
+                />
+              </div>
+            );
+          })}
+      </div>
 
       <div className="event-form__button">
-        <Button label="Submit Form" onClick={handleSubmit} />
+        <Button label="Create Event" onClick={handleRequirements} />
       </div>
     </div>
   );
