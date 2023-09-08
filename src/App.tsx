fc2f19627099981b4f2652de72d938704e125aa9
Tufiale <138790020+Tufiale@users.noreply.github.com>
2023-09-08 10:31:16 +0100

    49-feature-loading-page Added loading spinner package (#40)
    * Added loading spinner paclage
    
    * Removed dependency array
    
    * added the correct logic for the loader
    
    * added the spinner as a component
    
    ---------
    
    Co-authored-by: NDenheim <nicole.denheim@hotmail.co.uk>
    Co-authored-by: olirob93 <ollierobins93@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,61 +14,69 @@ import Account from "./pages/Account/Account";
 import Admin from "./pages/Admin/Admin";
 import { getEvents } from "./utils/firebaseSnapshots";
 import { Event } from "./types/types";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
 const App = () => {
   const [dbData, setDbData] = useState<Event[]>([]);
+  const [user, setUser] = useState<object>();
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getDbData();
-  }, []);
+  const auth = getAuth();
+  const navigate = useNavigate();
 
   const getDbData = async () => {
     const data = await getEvents();
     setDbData(data as Event[]);
+    setIsLoading(false);
   };
-  const [user, setUser] = useState<object>();
-
-  const auth = getAuth();
-  const navigate = useNavigate();
 
   useEffect(
     () => {
       onAuthStateChanged(auth, (user) => {
         if (user) {
           setUser(user);
+          setIsLoading(false);
         } else {
           navigate("/splash");
           return;
         }
       });
+      getDbData();
     }, // eslint-disable-next-line
     []
   );
 
   return (
     <>
-      <Routes>
-        {user ? (
-          <>
-            <Route path="/" element={<Home />} />
-            <Route path="/events" element={<Events eventData={dbData} />} />
-            <Route
-              path="/calendar"
-              element={<CalendarPage eventData={dbData} />}
-            />
-            <Route path="/about" element={<About />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/admin" element={<Admin />} />
-          </>
-        ) : (
-          <>
-            <Route path="splash" element={<SplashPage />} />
-            <Route path="/login" element={<Login setUser={setUser} />} />
-            <Route path="/register" element={<Register setUser={setUser} />} />
-          </>
-        )}
-        <Route path="*" element={<Error />} />
-      </Routes>
+      {isLoading === true ? (
+        <LoadingSpinner />
+      ) : (
+        <Routes>
+          {user ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/events" element={<Events eventData={dbData} />} />
+              <Route
+                path="/calendar"
+                element={<CalendarPage eventData={dbData} />}
+              />
+              <Route path="/about" element={<About />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="/admin" element={<Admin />} />
+            </>
+          ) : (
+            <>
+              <Route path="splash" element={<SplashPage />} />
+              <Route path="/login" element={<Login setUser={setUser} />} />
+              <Route
+                path="/register"
+                element={<Register setUser={setUser} />}
+              />
+            </>
+          )}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      )}
     </>
   );
 };
