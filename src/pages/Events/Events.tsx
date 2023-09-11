c4803db6a89d3eb0359d19a5b64754e3a4a4f8fa
maxswaine <108192224+maxswaine@users.noreply.github.com>
2023-09-11 10:18:29 +0100

    merge from main, added filter to sort all the events by ascending dates

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -2,7 +2,7 @@ import "./Events.scss";
 import Header from "../../components/Header/Header";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { ChangeEvent, useState } from "react";
-import { isAfter, format } from "date-fns";
+import { isAfter, format, compareAsc } from "date-fns";
 import { Event } from "../../types/types";
 import EventCard from "../../components/EventCard/EventCard";
 import Layout from "../../components/Layout/Layout";
@@ -30,6 +30,8 @@ const Events = ({ eventData }: EventsProps) => {
     const currentDate = new Date();
     return isAfter(event.date, currentDate);
   });
+
+  filterByDate.sort((a, b) => compareAsc(a.date, b.date));
 
   const filteredSearch = filterByDate.filter(
     (event: { name: string; category: string; description: string }) => {
